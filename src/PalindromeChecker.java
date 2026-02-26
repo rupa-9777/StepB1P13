370f4db5402b78e098eba36aebbc2e16f30955dd
rupa-9777 <rupasreeputta9999@gmail.com>
2026-02-26 15:53:59 +0530

    UC1: Add welcome message module

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
 
 public class PalindromeChecker {
+  public static void main(String[] args){
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter the string");
+      String input = scanner.nextLine();
+      Deque<Character> deque = new ArrayDeque<>();
+      for (char c : input.toCharArray()){
+          deque.addLast(c);
+      }
+      boolean isPalindrome = true;
+      while(deque.size() > 1){
+          if(!deque.removeFirst().equals(deque.removeLast())){
+              isPalindrome = false;
+              break;
+          }
+      }
+      System.out.println("Input :" + input);
+      System.out.println("Is Palindrome " + isPalindrome);
+      scanner.close();
+  }
 }
