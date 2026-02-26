65e3bd2c2f5d051893c1494d3d37973fa69e730c
rupa-9777 <rupasreeputta9999@gmail.com>
2026-02-26 15:09:55 +0530

    Implement palindrome checker in PalindromeChecker
    Added a main method to check if a string is a palindrome using a deque.

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,4 +1,24 @@
 package PACKAGE_NAME;
 
 public class PalindromeChecker {
+   public static void main(String[] args){
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter the string");
+      String input = scanner.nextLine();
+      Deque<Character> deque = new ArrayDeque<>();
+      for (char c : input.toCharArray()){
+          deque.addLast(c);
+      }
+      boolean isPalindrome = true;
+      while(deque.size() > 1){
+          if(!deque.removeFirst().equals(deque.removeLast())){
+              isPalindrome = false;
+              break;
+          }
+      }
+      System.out.println("Input :" + input);
+      System.out.println("Is Palindrome " + isPalindrome);
+      scanner.close();
+  }
+
 }
